57ee017a50844d1df48bb224f7bde035e8bf8a91
gladsongoms <104565172+gladsongoms@users.noreply.github.com>
2022-07-09 13:54:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,65 @@
-import streamlit as st 
-import joblib 
+ import streamlit as st
+import numpy as np
 
-#load the joblib model 
-model_nb = joblib.load('Iris')
+import joblib
 
-#user input 
-st.title("Iris Flower Classification ")
-ip = st.text_input(" Enter -(sepal_length	sepal_width	petal_length	petal_width	) in this format --eg.[[5.1, 3.5, 1.4, 0.2]] ")
 
-#predict if the entered message is spam or ham 
-op = model_nb.predict([ip])
-if st.button('PREDICT'):
-  st.title(op[0])  #prints the output as spam or ham  
 
+# loading the saved model
+loaded_model = joblib.load('Iris')
+
+
+# creating a function for Prediction
+
+def Iris_prediction(input_data):
+    
+
+    # changing the input_data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+    prediction = loaded_model.predict(input_data_reshaped)
+    print(prediction)
+
+    if (prediction[0] =='Iris-setosa'):
+      return 'Iris-setosa flower'
+    elif(prediction[0] =='Iris-versicolor'):
+      return 'Iris-versicolor flower'
+    else:
+      return 'Iris-virginica'
   
-                
+    
+  
+def main():
+    
+    
+    # giving a title
+    st.title('Iris flower Prediction Web App')
+    
+    
+    # getting the input data from the user
+    
+    
+    sepal_length= st.text_input('Length of Sepal')
+    sepal_width= st.text_input(' Enter sepal width')
+    petal_length= st.text_input('Enter Length of Petal')
+    petal_width= st.text_input('Enter petal width')
+    
+    
+    # code for Prediction
+    diagnosis = ''
+    
+    # creating a button for Prediction
+    
+    if st.button('Result'):
+        diagnosis = Iris_prediction([[sepal_length,	sepal_width,	petal_length,	petal_width	]])
+        
+       
+    st.success(diagnosis)
+    
+    
+if __name__ == '__main__':
+    main()
+    
